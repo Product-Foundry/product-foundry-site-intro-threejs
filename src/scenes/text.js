dad81df5acb0609c713be0aebb2cc311e3becf07
Andre Kampert <andre@product-foundry.com>
2017-05-08 12:14:39 +0200

    Resize text based on width

--- src/scenes/text.js
+++ src/scenes/text.js
@@ -6,6 +6,8 @@ import textFs from 'raw!glslify!../shaders/text.frag';
 
 import * as fontUtils from '../utils/font';
 
+import input from '../input';
+
 import buffer from 'three-buffer-vertex-data';
 import createText from 'three-bmfont-text';
 
@@ -26,19 +28,37 @@ class TextScene {
   }
 
   init(renderer, scene) {
+    this.scale = 1;
+
     this.renderer = renderer;
     this.scene = scene;
 
+    input.onResize.add((event) => this._onResize(event));
+
     return fontUtils.load({font: overpassBlackFnt, image: overpassBlackPng})
       .then((options) => {
-        this.initFont(options);
+        this.font = options.font;
+        this.texture = options.texture;
+        this.refreshFont();
+        this.refreshText();
       });
   }
 
-  initFont(options) {
+  _onResize(event) {
+    this.scale = event.width / event.height / 1.5;
+
+    this.refreshFont();
+    this.refreshText();
+  }
+
+  refreshFont() {
+    if (!!this.textAnchor) {
+      this.scene.remove(this.textAnchor);
+    }
+
     this.geometry = createText({
-      text: "test",
-      font: options.font,
+      text: "",
+      font: this.font,
       align: 'center',
       width: 800,
       lineHeight: 64
@@ -50,7 +70,7 @@ class TextScene {
       uniforms: {
         animate: {type: 'f', value: 1},
         iGlobalTime: {type: 'f', value: 0},
-        map: {type: 't', value: options.texture},
+        map: {type: 't', value: this.texture},
         color: {type: 'c', value: new THREE.Color('#ffffff')}
       },
       transparent: true,
@@ -60,35 +80,28 @@ class TextScene {
 
     this.text = new THREE.Mesh(this.geometry, this.material);
 
-    const textAnchor = new THREE.Object3D();
-    textAnchor.add(this.text);
+    this.textAnchor = new THREE.Object3D();
+    this.textAnchor.add(this.text);
 
     const mS = (new THREE.Matrix4()).identity();
-    // mS.elements[0] = 1;
-    mS.elements[5] = -1;
-    mS.elements[10] = -1;
-    textAnchor.applyMatrix(mS);
+    mS.elements[0] = this.scale;
+    mS.elements[5] = -this.scale;
+    mS.elements[10] = -this.scale;
+    this.textAnchor.applyMatrix(mS);
 
-    this.scene.add(textAnchor);
-
-    this.next();
+    this.scene.add(this.textAnchor);
   }
 
-  next() {
+  refreshText() {
     this.geometry.update(texts[this.nextIndex]);
-
-    this.nextIndex++;
-    if (this.nextIndex === texts.length) {
-      this.nextIndex = 0;
-    }
 
     const lines = this.geometry.visibleGlyphs.map(function (glyph) {
       return glyph.line
     });
 
     const lineCount = 1 + lines.reduce(function (a, b) {
-      return Math.max(a, b)
-    }, 0);
+        return Math.max(a, b)
+      }, 0);
 
     // for each quad, let's give it a vertex attribute with the line index
     const lineData = lines.map(function (line) {
@@ -109,6 +122,13 @@ class TextScene {
     this.text.position.y = layout.height / 2;
   }
 
+  nextText() {
+    this.nextIndex++;
+    if (this.nextIndex === texts.length) {
+      this.nextIndex = 0;
+    }
+  }
+
   render(delta) {
     this.time += delta;
     const duration = 8;
@@ -117,7 +137,8 @@ class TextScene {
 
     if (this.time > duration) {
       this.time = 0;
-      this.next();
+      this.nextText();
+      this.refreshText();
     }
   }
 }
